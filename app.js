0872f783cfa9ba17de053288f9c08c5c67bb6078
Dana Simmons <dsanalog@gmail.com>
2019-09-14 15:39:47 -0400

    Implement message handlers and responses
    Figured out how to receive events from the RTM api
    currently, messages are filtered based on regular expression
    and if the userID is mentioned, we send a response.
    I learned that the RTM api only recieves events from channels that
    the app has been added too. AND requies the channel:read scope

--- app.js
+++ app.js
@@ -6,20 +6,55 @@
 
 
 const { RTMClient } = require('@slack/rtm-api');
+const { IncomingWebhook } = require('@slack/webhook');
 const { WebClient } = require('@slack/web-api');
 
 //Load Enviornment variables
 require('dotenv').config();
 
-const token = process.env.API_TOKEN;
-const user_token = process.env.USER_TOKEN;
+const api_token = process.env.API_TOKEN;
+const bot_token= process.env.BOT_TOKEN;
+const radio_webhook = process.env.RADIO_WEBHOOK;
 
-rtm = new RTMClient(token);
-client = new WebClient(user_token);
+rtm = new RTMClient(bot_token);
+client = new WebClient(api_token);
 
-rtm.on('message', ( e ) => {
-  console.log(e);
+
+function parseMsg( text ) { // Parse messages with @ mentions
+  return text.match(/<@(?<to>.*)> (?<msg>.*)$/);
+}
+
+
+rtm.on('message', ( e ) =>{
+  console.log( e );
+  const parsedMsg = parseMsg( e.text );
+
+  console.log(parsedMsg);
+
+  if(parsedMsg && parsedMsg.groups.to === rtm.activeUserId){
+    //They are talking to us, do something
+    console.log("Sending reply...");
+    rtm.sendMessage(`I haven't learned that yet, ask @Dana KN4BEV about teaching me that`, e.channel );
+  }
+   
 });
+
+rtm.on('channel_joined', ( e  ) =>{
+  
+  rtm.sendMessage(`Hi I'm Hambone, I don't do much right now, but i'm happy to be here`,e.channel);
+
+});
+rtm.on('member_joined_channel', ( e ) => {
+  console.log(e);
+  rtm.sendMessage(`Welcome to <#${e.channel}> <@${e.user}>, tell us about yourself`,e.channel);
+});
+
+rtm.on('member_left_channel', ( e ) =>{
+
+  console.log( e );
+
+});
+
 rtm.on('connected', ( e ) => {
   console.log("connected and ready...");
   //Joining channels not working for some reason
