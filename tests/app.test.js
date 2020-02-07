46d12fbfeba3fa5a75e1d1017428519fbd87e78f
Dana Simmons <dsanalog@gmail.com>
2020-02-06 19:57:41 -0500

    Add tests for additional event types
    Why: Hambone responds to multiple types of events in various ways
    
    * ...
    
    Each event type should be tested to verify that it responds as expected
    and to verify that the correct handlers are fired.
    
    * ...

--- tests/app.test.js
+++ tests/app.test.js
@@ -12,7 +12,28 @@ const app = require('../app');
 describe('RTMClient responds to events',( ) =>{
   test('message handler is called when a message is recieved',( ) =>{
     RTMClient.fire('message',{text:"<@me> My message"} )
-    expect(RTMClient.HANDLERS['message']).toHaveBeenCalled()
+    expect(RTMClient.HANDLERS['message']).toHaveBeenCalled();
+
+    RTMClient.fire('message',{text:'<@me> /help '})
+
+  });
+  
+  test(' responds with a help message', ( ) =>{
+
+    RTMClient.fire('message',{text:'<@me> /help '});
+
+    expect(RTMClient.sendMessage).toHaveBeenCalled();
 
   })
+  test('channel join handler is called on channel join', ( ) => {
+    RTMClient.fire('channel_joined', { 'test': 'test' });
+
+    expect(RTMClient.HANDLERS['channel_joined']).toHaveBeenCalled();
+  })
+
+  test('member joined handler is called on channel join', ( ) => {
+    RTMClient.fire('member_joined_channel', { 'test': 'test' });
+
+    expect(RTMClient.HANDLERS['member_joined_channel']).toHaveBeenCalled();
+  });
 })
